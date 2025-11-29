f05000bc52d595b9f9e43e9feefbff22987fa971
Juanjo <juanjobenavides314@gmail.com>
2025-11-29 10:11:24 -0500

    feat: add function greet

--- feat.ts
+++ feat.ts
@@ -1,6 +1,10 @@
 import { calculate } from "./functions"
-
+import { greet as greetGa } from "./function-2"
 export function myFunctionFeat() {
     const getNumberSum = calculate(0);
     return getNumberSum + 1; // 2
+}
+
+export function greet() {
+    return greetGa()
 }
