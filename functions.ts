33410037b86116b37b21ce373c038db5406a6ae8
Juanjo <juanjobenavides314@gmail.com>
2025-12-13 17:19:40 -0500

    feat: add tutki tuki function

--- functions.ts
+++ functions.ts
@@ -1,3 +1,7 @@
 export function calculate(numberA:number) {
     return 1 + 1 + numberA
 }
+
+export function tukiTuki() {
+    return "tuki tuki papapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapapa"
+}
